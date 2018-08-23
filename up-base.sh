1729e0f02c1785e4757cada54776a21eea9c09a2
Kent James <kent@caspia.com>
2018-08-23 00:21:44 -0700

    allow nginx-proxy to be replaced by offlineweb

--- up-base.sh
+++ up-base.sh
@@ -2,12 +2,15 @@
 
 # script file to start base beluga containers.
 
+echo "IP_ADDR is ${IP_ADDR}"
+echo "DNSMASQ_EXTRA is ${DNSMASQ_EXTRA}"
+
 # remove and recreate the beluga network
 docker network rm beluga 2> /dev/null
 docker network create \
   --subnet=172.20.0.0/20 \
   --ip-range=172.20.1.0/24 \
-  -o "" \
+  -o "com.docker.network.bridge.enable_icc=true" \
   beluga
 
 # default for DOMAIN is .ed"
@@ -18,14 +21,19 @@ echo "DOMAIN is $DOMAIN"
 export DOMAIN=$DOMAIN
 
 # nginx-proxy: maps url names to specific docker containers
-docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro --name nginx-proxy \
-  --network="beluga" --restart=always jwilder/nginx-proxy:alpine
+# Only install if this is the webhandler
+if [ -z "$WEBHANDLER" ] || [ "$WEBHANDLER" == "${nginx-proxy}" ]; then
+  docker run -d -p 80:80 --ip="172.20.0.102" \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    --name nginx-proxy --network="beluga" \
+    --restart=always jwilder/nginx-proxy:alpine
+fi
 
 # dnsmasq: DNS server returning the base IP address for any address in a domain.
 if [ -z "$OFFLINE" ]; then
   docker build -t caspia/dnsmasq:alpine ./dnsmasq
 fi
-echo "IP_ADDR is ${IP_ADDR}"
-echo "DNSMASQ_EXTRA is ${DNSMASQ_EXTRA}"
-docker run -p 53:53/tcp -p 53:53/udp -d --network="beluga" --restart=always --cap-add NET_ADMIN \
-  --name dnsmasq caspia/dnsmasq:alpine "-A" "/${DOMAIN}/${IP_ADDR}" "${DNSMASQ_EXTRA}"
+docker run -p 53:53/tcp -p 53:53/udp -d \
+ --network="beluga" --ip="172.20.0.101" \
+ --restart=always --cap-add NET_ADMIN \
+ --name dnsmasq caspia/dnsmasq:alpine "-A" "/${DOMAIN}/${IP_ADDR}" "${DNSMASQ_EXTRA}"
